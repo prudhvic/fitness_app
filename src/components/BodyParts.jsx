dcab6eaa0ff08559e69a876ce88a5032e1756f29
prudhvic <prudhvichinnam101@gmail.com>
2022-09-29 21:07:05 +0530

    key changes

--- src/components/BodyParts.jsx
+++ src/components/BodyParts.jsx
@@ -4,13 +4,25 @@ import gymImg from "../assets/icons/gym.png";
 import styles from "./BodyParts.module.css";
 const BodyParts = () => {
   let { bodyParts, fetchByBodyPart } = useExerciseContext();
-
+  let bodyImages = [
+    "All",
+    "back",
+    "cardio",
+    "chest",
+    "Lower Arms",
+    "Lower Legs",
+    "neck",
+    "Shoulders",
+    "Upper Arms",
+    "Upper Legs",
+    "waist",
+  ];
   return (
     <div className={styles.container}>
       <div className={styles.parts}>
         {bodyParts.map((part, index) => (
           <div key={index} onClick={() => fetchByBodyPart(part)}>
-            <img src={`/images/${part}.jpg`} alt="gym" />
+            <img src={`/images/${bodyImages[index]}.jpg`} alt={part} />
             <h3>{part}</h3>
           </div>
         ))}
