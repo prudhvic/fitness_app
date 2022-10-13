2c0940b116f142f9c5ecd4e674a4d91cfed23611
ajayreddyg <ajayreddy13102000@gmail.com>
2022-10-13 15:53:38 +0530

    initial

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,9 +2,9 @@ export const ExerciseOptions = {
   method: "GET",
   url: "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
   headers: {
-    "X-RapidAPI-Key": "33f6a3bec4msh87f6cacfdc7720fp15110ejsnc413b054ce90",
-    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-  },
+    'X-RapidAPI-Key': '3ac3d25eddmsh3b8983860bdc838p1afd35jsn9c391aec7556',
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+  }
 };
 export const ExerciseVideoOptions = {
   method: "GET",
